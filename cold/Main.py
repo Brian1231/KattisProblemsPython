a539e399a2e51d9b6f8382b51fc55b48752c15fc
Brian1231 <brianoleary1995@gmail.com>
2017-05-14 20:33:12 +0100

    -

--- cold/Main.py
+++ cold/Main.py
@@ -1,22 +1,11 @@
-#f = open("coldTest.txt","r")
-
 
 #Counts number of neg numbers in file
-count = 0;
-with open("coldTest.txt", "r") as f:
-    for line in f:
-        for s in line.split(' '):
-            num = int(s) #cast s to int
-            if num < 0 :
-                count = count + 1
-                print ("Count: %s" % count)
-print ("Total: %s" % count)
 
-
-import sys
- 
-for line in sys.stdin:
-    ab = line.split()
-    a = int(ab[0])
-    b = int(ab[1])
-    print(abs(a-b))
+cases = int(input())
+numbers = input().split(" ")
+count = 0
+for i in numbers:
+    if (int(i) < 0):
+        count += 1
+        
+print(count)
